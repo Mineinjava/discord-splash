d9acae8030cdfa9a9a427e90761e20e3e30f2af5
Mineinjava <fbisurveillancevan525178@gmail.com>
2021-02-06 11:33:15 -0800

    Update to V0.5-ALPHA
    - added more docstrings
    - added examples
    - added an `await... ...reply()` function

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # discord-splash
-An API wrapper for Discord's slash commands. 
+An API wrapper for Discord's slash commands.
+ 
+### Docs:
+coming soon™—use docstings for now.
+
+### **NOTE:**
+you are required to make the slash commands via the Discord API. ***Currently* this is not possible via this API wrapper.**
+### Examples:
+*Simple bot that responds with "hi"*
+```python
+from discordSlashCommands import Run
+import discordSlashCommands
+
+@discordSlashCommands.command(name='hello')
+async def hello(data):
+    await data.respond(discordSlashCommands.ReactionResponse("hi"))
+
+
+Run('TOKEN')
+```
+
+*Presence Example*
+```python
+from discordSlashCommands import Run, Presence
+import discordSlashCommands
+
+x = Presence(text='testing', presenceType=5)
+
+Run('TOKEN', x)
+```
--- discordSlashCommands/__init__.py
+++ discordSlashCommands/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 import json
+import aiohttp
 
 import discordSlashCommands.resources.opcodes as op
 import traceback
@@ -8,7 +9,29 @@ import traceback
 commands = {}
 
 
-class Presence():
+class Presence:
+    """Presence data used when connecting to Gateway
+    param presenceType (int): type of presence to use (DiscordActivityType Integer ([Discord Dev Docs](https://discord.com/developers/docs/topics/gateway#activity-object-activity-types))
+
+        0	Game	Playing {text}
+
+        1	Streaming	Streaming {text}
+
+        2	Listening	Listening to {text}
+
+        4	Custom	{emoji}  {text}
+
+        5	Competing	Competing in {text}
+
+        **note**: Streaming URL's currently do not work
+                  Custom emojis have not been implemented in this API wrapper
+
+    param text (str):
+        text of status to use
+        see {text} above
+
+        """
+
     def __init__(self, presenceType: int, text: str):
         self.type = presenceType
         self.text = text
@@ -20,26 +43,51 @@ class Presence():
         return self.text
 
 
-class ReactionResponse():
-    def __init__(self, content: str = None, isEphemeral: bool = False, responseType: int = 4):
-        pass
+class InvalidTypeException(Exception):
+    pass
 
-class Member():
+
+class ReactionResponse():
+    def __init__(self, content: str, isEphemeral: bool = False, responseType: int = 4):
+        if not responseType in [1, 2, 3, 4, 5]:
+            raise InvalidTypeException(
+                f"responseType {responseType} is invalid. See https://discord.com/developers/docs/interactions/slash-commands#interaction-response-interactionresponsetype for more info.")
+        self.jsonContent = {
+            "type": responseType,
+            "data": {
+                "content": str(content)
+            }
+        }
+        if isEphemeral:
+            self.jsonContent['data']['flags'] = 64
+
+    @property
+    def json(self):
+        return self.jsonContent
+
+
+class Member:
+    """Represents a discord member
+    currently read-only"""
+
     def __init__(self, memberJson):
         self.memberJson = memberJson
+
     @property
     def avatar(self):
         return self.memberJson['avatar']
+
     @property
     def id(self):
         return self.memberJson['id']
+
     @property
     def username(self):
         return self.memberJson['username']
+
     @property
     def discriminator(self):
         return self.memberJson['id']
-
 
 
 class ReactionData():
@@ -49,22 +97,49 @@ class ReactionData():
     @property
     def guild_id(self):
         return self.jsonData["guild_id"]
+
     @property
     def id(self):
         return self.jsonData['id']
     @property
+    def token(self):
+        return self.jsonData['token']
+    @property
     def type(self):
         return int(self.jsonData['type'])
+
     @property
     def user(self):
         return Member(self.jsonData['member']['user'])
 
+    @property
+    def options(self):
+        return self.jsonData['data']['options']
+
+    @property
+    def json(self):
+        return self.jsonData
+
+    async def respond(self, data: ReactionResponse):
+        async with aiohttp.ClientSession() as session:
+            print("jsondata", self.jsonData)
+            async with session.post(f'https://discord.com/api/v8/interactions/{self.jsonData["d"]["id"]}/{self.jsonData["d"]["token"]}/callback', json=data.json) as resp:
+                pass
+
 
 class Run():
     """Runs the bot using the token
-    parameters:
+
+    **parameters:**
+
     token (str): token of the bot to run
-    presence: """
+    presence: (splash.Presence): Presence of the bot, activated upon connection to gateway
+
+    **Example:**
+
+    `Run('TOKEN', Presence(text='testing', presenceType=5))`
+
+    """
 
     def __init__(self, token: str, presence: Presence = None):
         self.interval = None
@@ -131,7 +206,7 @@ class Run():
                     event_name = data['d']['data']['name']
                     try:
                         function = commands[event_name]
-                        await function(data)
+                        await function(ReactionData(data))
                     except KeyError:
                         try:
                             raise UnregisteredCommandException(
